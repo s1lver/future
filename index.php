619d163e4c96123b0e72b891481dc0874ff02018
Evgeniy Moiseenko <s1lverhron@gmail.com>
2016-05-30 15:39:14 +0500

    Added index file

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @user Moiseenko
+ * @date 30.05.2016
+ * @time 14:53
+ * @copyright Future CMF
+ */
+
+require_once 'core/models/Application.php';
+
+(new future\core\models\Application)->init();
