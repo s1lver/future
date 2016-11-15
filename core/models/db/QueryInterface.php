7d611edaaafe35c9e4879cb1c78832daab2e9fdd
Evgeniy Moiseenko <s1lverhron@gmail.com>
2016-11-15 11:15:58 +0300

    Интерфейс построителя запросов

---
+++ core/models/db/QueryInterface.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @author Evgeniy M
+ * @link   github.com/s1lver
+ */
+
+namespace future\core\models\db;
+
+/**
+ * Interface QueryInterface
+ * @package future\core\models\db
+ */
+interface QueryInterface
+{
+	public function where($condition);
+}
