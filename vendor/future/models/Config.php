4f6fe43064b8922e6c32dcd40a9692893bc06d39
Evgeniy Moiseenko <s1lverhron@gmail.com>
2018-07-12 18:53:30 +0300

    Added config class

---
+++ vendor/future/models/Config.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Модель конфигурации
+ *
+ * @user Moiseenko
+ * @date 12.07.2018
+ * @time 18:51
+ * @copyright Future CMF
+ */
+
+namespace future\models;
+
+/**
+ * Class Config
+ * @package future\models
+ */
+class Config
+{
+	public const VERSION = '0.0.0.1';
+}
